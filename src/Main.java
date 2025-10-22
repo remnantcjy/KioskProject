a0046b11494cc72036f5d4adb0549a74ca56c632
remnantcjy <remnantcjy@gmail.com>
2025-10-22 12:55:27 +0900

    feat: 키오스크 필수기능 Lv1 구현 - Scanner를 통해 사용자로부터 메뉴번호 입력 및 메뉴출력 구현

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,46 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("키오스크 프로젝트 만들기 !");
+
+        Scanner sc = new Scanner(System.in);
+
+        // 사용자 입력 변수 선언
+        String userChoice;
+
+        while (true) {
+            System.out.println("[ BURGERKING \uD83D\uDC51 MENU ]\n");
+            System.out.println("1. 통새우와퍼       | W 8.9  | 통새우, 스파이시 토마토 소스, 순쇠고기 패티가 들어간 통새우와퍼");
+            System.out.println("2. 몬스터와퍼       | W 10.3 | 디아블로 소스, 순쇠고기 패티, 치킨 패티가 들어간 몬스터와퍼");
+            System.out.println("3. 콰트로치즈와퍼    | W 8.7  | 4가지 치즈, 순쇠고기 패티가 들어간 치즈와퍼");
+            System.out.println("4. 트러플머쉬룸와퍼  | W 9.8   | 트러플 향, 4가지 버섯이 들어간 트러플머쉬룸와퍼");
+            System.out.println("0. 종료           | 종료");
+
+            System.out.print("주문하실 메뉴 번호를 입력해주세요(0번은 종료): ");
+            userChoice = sc.next();
+
+            if ("0".equalsIgnoreCase(userChoice))
+                break;
+
+            switch (userChoice) {
+                case "1":
+                    System.out.println("1. 통새우와퍼       | W 8.9  | 통새우, 스파이시 토마토 소스, 순쇠고기 패티가 들어간 통새우와퍼\n");
+                    break;
+                case "2":
+                    System.out.println("2. 몬스터와퍼       | W 10.3 | 디아블로 소스, 순쇠고기 패티, 치킨 패티가 들어간 몬스터와퍼\n");
+                    break;
+                case "3":
+                    System.out.println("3. 콰트로치즈와퍼    | W 8.7  | 4가지 치즈, 순쇠고기 패티가 들어간 치즈와퍼\n");
+                    break;
+                case "4":
+                    System.out.println("4. 트러플머쉬룸와퍼  | W 9.8   | 트러플 향, 4가지 버섯이 들어간 트러플머쉬룸와퍼\n");
+                    break;
+                default:
+                    System.out.println("잘못된 메뉴 번호입니다. 다시 입력해주세요.\n");
+                    break;
+            }
+        }
+
+
     }
 }
