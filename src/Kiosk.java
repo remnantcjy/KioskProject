f40db77714ee6ad43ea6bc501f0d6f2c4f4ad295
remnantcjy <remnantcjy@gmail.com>
2025-10-22 15:32:44 +0900

    feat: 키오스크 필수기능 Lv3 구현
    - Kiosk 클래스: start() 기능 구현
    - start() 기능 상세:
      - 콘솔에 햄버거 메뉴 출력
      - 사용자 입력을 받아 메뉴 선택 및 상세 정보 출력
      - 0 입력 시 프로그램 종료
      - 유효성 검증: 숫자 외 입력 시 예외처리 및 재입력 유도

--- src/Kiosk.java
+++ src/Kiosk.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 // 키오스크 프로그램의 메뉴를 관리하고 사용자 입력을 처리하는 클래스 - <MenuItem> List 필드, start()
 public class Kiosk {
@@ -19,6 +20,41 @@ public class Kiosk {
     // 기능
     //main 함수에서 관리하던 입력과 반복문 로직은 이제 start 함수를 만들어 관리
     public void start() {
+        Scanner sc = new Scanner(System.in);
 
+        // 메뉴 출력 - 반복문을 활용해 List 안에 있는 MenuItem을 하나씩 출력
+        System.out.println("[ BURGERKING \uD83D\uDC51 MENU ]");
+        for (MenuItem menu : menuItems) {
+            System.out.println(menu);
+        }
+
+        // 사용자 입력 변수 선언
+        String userChoice;
+
+        while (true) {
+
+            // 유효성 검증 메서드 - 숫자 외의 값 입력시 예외처리 및 재입력
+            try {
+                System.out.print("\n주문하실 메뉴의 번호를 입력해주세요(0번은 종료): ");
+                // 입력된 숫자에 따른 처리
+                userChoice = sc.next();
+
+                // 리스트에 인덱스로 접근하기 위해 정수로 변환
+                int index = Integer.parseInt(userChoice);
+
+                if ("0".equalsIgnoreCase(userChoice)) {
+                    break;
+                } else if(menuItems.size() < index) {
+                    // 메뉴아이템리스트의 사이즈보다 인덱스가 클 때 재입력 받기
+                    System.out.println("잘못된 메뉴 번호입니다. 다시 입력해주세요.\n");
+                } else {
+                    // 해당 인덱스의 값 출력하기
+                    System.out.println("\n선택한 메뉴: " + menuItems.get(index-1));
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("잘못된 입력 ❌ 메뉴 번호를 입력해주세요 ❗\uFE0F");
+            }
+
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -17,37 +17,10 @@ public class Main {
         //  List<MenuItem> menuItems 는 Kiosk 클래스 생성자를 통해 값을 할당합니다.
         //  Kiosk 객체를 생성하고 사용하는 main 함수에서 객체를 생성할 때 값을 넘겨줍니다.
 
-        // 키오스크 필드 선언 및 객체 생성
+        // 키오스크 필드 선언 및 객체 생성 - 버거류 리스트 값을 할당!
         Kiosk kiosk = new Kiosk(burgerItems);
 
-        Scanner sc = new Scanner(System.in);
-
-        // 메뉴 출력 - 반복문을 활용해 List 안에 있는 MenuItem을 하나씩 출력
-        System.out.println("[ BURGERKING \uD83D\uDC51 MENU ]");
-        for (MenuItem menu : burgerItems) {
-            System.out.println(menu);
-        }
-
-        // 사용자 입력 변수 선언
-        String userChoice;
-
-        while (true) {
-            System.out.print("\n주문하실 메뉴의 번호를 입력해주세요(0번은 종료): ");
-            // 입력된 숫자에 따른 처리
-            userChoice = sc.next();
-
-            // 리스트에 인덱스로 접근하기 위해 정수로 변환
-            int index = Integer.parseInt(userChoice);
-
-            if ("0".equalsIgnoreCase(userChoice)) {
-                break;
-            } else if(burgerItems.size() < index) {
-                // 메뉴아이템리스트의 사이즈보다 인덱스가 클 때 재입력 받기
-                System.out.println("잘못된 메뉴 번호입니다. 다시 입력해주세요.\n");
-            } else {
-                // 해당 인덱스의 값 출력하기
-                System.out.println("\n선택한 메뉴: " + burgerItems.get(index-1));
-            }
-        }
+        // 키오스크 주문 기능
+        kiosk.start();
     }
 }
